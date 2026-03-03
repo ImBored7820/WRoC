15f5e25cab7760c86f9390d1960fdddc48ca39b6
Musa <119911104+ImBored7820@users.noreply.github.com>
2026-03-03 14:04:37 -0800

    TS Changes, Started sprite (player) creation

---
+++ map.js
@@ -0,0 +1,59 @@
+var canvas;
+var ctx;
+var mapCanvas;
+var mapCtx;
+var tileWidth = 20;
+var tileHeight = 20;
+var rows = 20;
+var cols = 20;
+var tileColors = {
+    0: "black",
+    1: "gray",
+    2: "brown",
+    3: "lightgray"
+};
+var map = [
+    0, 2, 2, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, // Center
+    0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, // Center 
+    0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 3, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 3, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 3, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0,
+];
+var drawMap = function () {
+    for (var row = 0; row < rows; row++) {
+        for (var col = 0; col < cols; col++) {
+            var index = row * cols + col;
+            mapCtx.fillStyle = tileColors[map[index]];
+            mapCtx.fillRect(col * tileWidth, row * tileHeight, tileWidth, tileHeight);
+        }
+    }
+};
+var refreshRate = function () {
+    ctx.drawImage(mapCanvas, 0, 0);
+    window.requestAnimationFrame(refreshRate);
+};
+window.onload = function () {
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+    mapCanvas = document.createElement("canvas");
+    mapCanvas.width = cols * tileWidth;
+    mapCanvas.height = rows * tileHeight;
+    mapCtx = mapCanvas.getContext("2d");
+    drawMap();
+    window.requestAnimationFrame(refreshRate);
+};
