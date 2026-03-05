7c7c045f9b19da9522f4168734db1904c3b48440
musa <mohammedmusaali549@gmail.com>
2026-03-04 16:56:23 -0800

    Commenting Finished, Player.ts updates

--- script.ts
+++ script.ts
@@ -20,36 +20,39 @@ import {Player} from "./game/player.js";
 function onStart() {
     const canvas = document.getElementById("canvas") as HTMLCanvasElement;
     const ctx = canvas.getContext("2d");
-    const pixelMap = document.createElement("canvas");
+    const pixelMap = document.createElement("canvas"); // map from map.ts
     const mapCtx = pixelMap.getContext("2d");
 
-    /* Tests */ const map = new Image(); map.src = "bg.png"; /* Tests */
+    /* Tests */ const map = new Image(); map.src = "./assets/bg.png"; /* Tests */
 
-    const player = new Player();
-    player.movementKeys();
+    const player = new Player(); // Creates a new player then enables checking
+    player.movementKeys(); // if movement keys are pressed
 
+    // So the purpose of this function is to make a canvas that fits the screen
+    // no matter what the screen size is
     function resize() {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
         pixelMap.width = canvas.width;
         pixelMap.height = canvas.height;
-        if (mapCtx) drawMap(mapCtx);
+        drawMap(mapCtx);
     }
 
-    window.addEventListener("resize", resize);
-    resize();
+    window.addEventListener("resize", resize); // Watches for resizing of browser
+    resize(); // This is for the first time, on load it "resizes" the canvas"
 
     function refreshRate(){
-        if (!ctx) return; // Safety check
-
-        const camX = -player.x + canvas.width / 2;
-        const camY = -player.y + canvas.height / 2;
+        if (!ctx) return; // Safety check, TypeScript thing
+        player.update();
+        // Logic for the "camera", moves the map instead of the player, creating
+        // a visual effect of a camera following the player around
+        const camX = (canvas.width / 2) - player.x;
+        const camY = (canvas.height / 2) - player.y;
 
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-
         ctx.save();
-        ctx.translate(camX, camY);
-
+        ctx.translate(camX, camY); // Implements the previous map moving logic
+                                   // into the canvas
         ctx.drawImage(pixelMap, 0, 0);
 
         player.update();
@@ -57,10 +60,12 @@ function onStart() {
 
         ctx.restore();
 
-        window.requestAnimationFrame(refreshRate);
+        window.requestAnimationFrame(refreshRate); // Recursion so changes
+                                                   // happen in real time
     }
     window.requestAnimationFrame(refreshRate);
 }
 
-window.addEventListener("DOMContentLoaded", onStart);
+window.addEventListener("DOMContentLoaded", onStart); // Once html is parsed,
+                                                            // loads objects
 
